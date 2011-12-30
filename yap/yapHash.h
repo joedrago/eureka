190dc7d3cef6ebe4d04a2a59b174ff33f22d98d7
Joe Drago <joedrago@gmail.com>
2011-12-30 04:11:26 +0000

    added missing hash file
    git-svn-id: svn://drago.cc/yap/trunk@102 93c65507-1ebd-42ea-85eb-edb014e0270b

---
+++ yap/yapHash.h
@@ -0,0 +1,31 @@
+#ifndef YAPHASH_H
+#define YAPHASH_H
+
+#include "yapTypes.h"
+
+typedef struct yapHashEntry
+{
+    yU32 hash;
+    char *key;
+    void *value;
+    struct yapHashEntry *next;    
+} yapHashEntry;
+
+typedef struct yapHash
+{
+    int count;
+    int width; // number of ptrs in table array
+    yapHashEntry **table;
+} yapHash;
+
+yapHash *yapHashCreate(int sizeEstimate);
+void yapHashClear(yapHash *yh, yapDestroyCB cb);
+void yapHashDestroy(yapHash *yh, yapDestroyCB cb);
+
+typedef void (*yapIterateCB1)(void *arg1, yapHashEntry *entry);
+void yapHashIterateP1(yapHash *yh, yapIterateCB1 cb, void *arg1); // One prefixed argument: cb(arg1, p)
+
+void **yapHashLookup(yapHash *yh, const char *key, yBool create);
+void yapHashDelete(yapHash *yh, const char *key, yapDestroyCB cb);
+
+#endif
