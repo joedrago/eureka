5456a1bff77fb31fdf4d7aeb2f52245ca39b0201
Joe Drago <joedrago@gmail.com>
2012-01-17 05:29:29 +0000

    Added Boost License info and README, fixed line endings
    git-svn-id: svn://drago.cc/yap/trunk@126 93c65507-1ebd-42ea-85eb-edb014e0270b

--- yap/core/yapVariable.h
+++ yap/core/yapVariable.h
@@ -1,3 +1,10 @@
+// ---------------------------------------------------------------------------
+//                   Copyright Joe Drago 2010 - 2011.
+//         Distributed under the Boost Software License, Version 1.0.
+//            (See accompanying file LICENSE_1_0.txt or copy at
+//                  http://www.boost.org/LICENSE_1_0.txt)
+// ---------------------------------------------------------------------------
+
 #ifndef YAPVARIABLE_H
 #define YAPVARIABLE_H
 
