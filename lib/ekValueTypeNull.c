9e8896c5a1b985573a13cad019ea1f5a261b6cc6
Joe Drago <joedrago@gmail.com>
2015-04-09 00:00:05 -0700

    Make it legal to attempt to iterate on null. It just loops zero times.
    This makes iterating on files super awesome.

--- lib/ekValueTypeNull.c
+++ lib/ekValueTypeNull.c
@@ -8,6 +8,8 @@
 #include "ekValue.h"
 
 #include "ekContext.h"
+#include "ekFrame.h"
+#include "ekMap.h"
 #include "ekValueType.h"
 
 #include <stdio.h>
@@ -29,11 +31,41 @@ static void nullFuncDump(struct ekContext *E, ekDumpParams *params, struct ekVal
     ekStringConcat(E, &params->output, NULL_STRING_FORM);
 }
 
+static ekU32 nullIterator(struct ekContext *E, ekU32 argCount)
+{
+    ekFrame *frame = ekArrayTop(E, &E->frames);
+    ekAssert(frame->closure && frame->closure->closureVars);
+    ekAssert(argCount == 0);
+    ekContextPopValues(E, argCount);
+    ekContextReturn(E, ekValueNullPtr);
+}
+
+static ekU32 nullCreateIterator(struct ekContext *E, ekU32 argCount)
+{
+    ekValue *thisValue = NULL;
+    ekValue *closure;
+
+    if(!ekContextGetArgs(E, argCount, "0", &thisValue))
+    {
+        return ekContextArgsFailure(E, argCount, "null iterator missing argument");
+    }
+
+    closure = ekValueCreateCFunction(E, nullIterator);
+    closure->closureVars = ekMapCreate(E, EMKT_STRING);
+    ekContextReturn(E, closure);
+}
+
+static ekCFunction *nullFuncIter(struct ekContext *E, struct ekValue *p)
+{
+    return nullCreateIterator;
+}
+
 void ekValueTypeRegisterNull(struct ekContext *E)
 {
     ekValueType *type   = ekValueTypeCreate(E, "null", '0');
     type->funcToString   = nullFuncToString;
     type->funcDump       = nullFuncDump;
+    type->funcIter       = nullFuncIter;
     ekValueTypeRegister(E, type);
     ekAssert(type->id == EVT_NULL);
 }
